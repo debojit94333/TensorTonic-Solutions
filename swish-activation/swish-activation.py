02e62fe2fed24bb5e513ddedc7dbdde908ccd980
Debojit Roy <139033817+debojit94333@users.noreply.github.com>
2026-08-05 22:43:31 +0530

    Solve swish-activation

--- swish-activation/swish-activation.py
+++ swish-activation/swish-activation.py
@@ -5,5 +5,6 @@ def swish(x):
     Implement Swish activation function.
     """
     # Write code here
-    x = np.array(x)
-    return x * (1 / (1 + np.exp(-x)))
+    x = np.asarray(x)
+    mu_x = 1 / (1 + np.exp(-x))
+    return x * mu_x
